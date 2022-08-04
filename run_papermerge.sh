294430bb8578ee85913df4b331932da79ed3925b
Jonathan Chen <force@xe2.io>
2022-08-03 23:45:02 -0500

    create volume mount folders and set permissions if they don't exist

--- run_papermerge.sh
+++ run_papermerge.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+# Note: uid/gid on the host may not match those in-container when user/group id mapping is used (e.g. rootless docker, podman)
+uid=1000
+gid=1000
+
+# Create directories for papermerge persistence and importer/watch directory
+for dir in config data watchdir; do
+    [ -d "$dir" ] || ( mkdir "$dir" && chown -R "$uid:$gid" "$dir" )
+done
+
 ce=$(which podman 2>/dev/null) || ce=$(which docker 2>/dev/null) || ce='echo -e Please install a container engine to run:\n    <ce> '
-gitroot=$(git rev-parse --show-cdup 2>/dev/null || pwd)
+gitroot=$(git rev-parse --show-toplevel 2>/dev/null || pwd)
 
 "$ce" run \
   --name=papermerge \
   --rm \
-  -e PUID=1000 \
-  -e PGID=1000 \
+  -e PUID="$uid" \
+  -e PGID="$gid" \
   -e TZ=UTC \
   -p 8000:8000 \
   -v "$gitroot/config":/config \
